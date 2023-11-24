d9aee408a1ad0e525705a6d2455c2d96e5c1806e
Chris Suzukida <47435313+csuzukida@users.noreply.github.com>
2023-11-24 11:54:56 -0800

    Add styling to instructions

--- src/client/components/Instructions.tsx
+++ src/client/components/Instructions.tsx
@@ -1,68 +1,82 @@
 import { useNavigate } from 'react-router-dom';
-import { Button, Typography } from '@mui/joy';
-import { Box } from '@mui/material';
+import { Button, Typography, Box, List, ListItem } from '@mui/material';
 import { boxStyle } from '../utils';
 
-const instructionsBoxStyle = {
-  boxSizing: 'border-box',
-  padding: '1.5rem',
+const instructionBoxStyle = {
   ...boxStyle,
+  overflowY: 'auto',
+  '& ul': {
+    padding: 0,
+    listStyleType: 'disc',
+    marginLeft: '1.5rem',
+  },
+  '& li': {
+    display: 'list-item',
+  },
 };
 
 const Instructions = () => {
   const navigate = useNavigate();
-  const logicSegments = [
-    'The computer will generate a random 4 digit combination.',
-    'You have 10 tries to guess the combination!',
-    'After each guess, the code checker will tell you:',
-    '- The number of correct digits in the correct place',
-    '- The number of correct digits in the incorrect place',
-    '- If you did not have any correct digits',
-    'Use your logical thinking prowess to solve the combination!',
-  ];
-
-  const settingSegments = [
-    'Find the game too easy or difficult?',
-    'Easily adjust the game settings to your liking:',
-    '- Increase or decrease the length of the combination',
-    '- Increase or decrease the amount of guesses',
-    '- Enable timer mode to also race against a clock',
-    '- Adjust the timer for whatever difficulty level suits you',
-  ];
-
-  const accountSegments = [
-    'Lastly, you can create an account with us!',
-    "Don't worry, your information is secure",
-    ' and we will never sell your information.',
-    'Each win gives you 10 points',
-    '+ 1 point for every guess you had remaining',
-    '(only when set to 10 guesses or less).',
-    'Add your friends and compete for',
-    'bragging rights in a local leaderboard!',
+  const instructionSections = [
+    {
+      title: 'How to Play',
+      content: [
+        'The computer will generate a random 4-digit combination from 0-9',
+        'You have 10 tries to guess the combination correctly',
+        'After each guess, you will receive feedback on: the number of correct digits in total and the number of correct digits in the correct place, or if you got all the digits incorrect',
+      ],
+    },
+    {
+      title: 'Game Settings',
+      content: [
+        'Adjust the difficulty level to your preference!',
+        'Change the number of digits in the combination',
+        'Change the range that each digit can be (0-9)',
+        'Set the maximum number of guesses allowed',
+        'Toggle the timer and set your challenge',
+      ],
+    },
+    {
+      title: 'Create an Account',
+      content: [
+        'Sign up for a personalized experience and track your scores.',
+        'Compete with friends and climb the leaderboard!',
+        'Rest assured, your privacy is protected.',
+      ],
+    },
   ];
 
   return (
-    <Box sx={instructionsBoxStyle}>
-      <Button onClick={() => navigate('/')}>Home</Button>
-      <Typography level="body-lg" sx={{ textAlign: 'center', width: '100%', marginTop: '1rem' }}>
-        Instructions
-      </Typography>
-      {logicSegments.map((segment, index) => (
-        <Typography key={index} level="body-sm" sx={{ textAlign: 'center', width: '100%' }}>
-          {segment}
-        </Typography>
-      ))}
-      <br />
-      {settingSegments.map((segment, index) => (
-        <Typography key={index} level="body-sm" sx={{ textAlign: 'center', width: '100%' }}>
-          {segment}
-        </Typography>
-      ))}
-      <br />
-      {accountSegments.map((segment, index) => (
-        <Typography key={index} level="body-sm" sx={{ textAlign: 'center', width: '100%' }}>
-          {segment}
-        </Typography>
+    <Box sx={instructionBoxStyle}>
+      <Box sx={{ alignSelf: 'flex-start', marginLeft: '2rem', marginTop: '2rem' }}>
+        <Button variant="contained" onClick={() => navigate('/')}>
+          Home
+        </Button>
+      </Box>
+      {instructionSections.map((section, index) => (
+        <Box
+          key={index}
+          sx={{ width: '100%', marginLeft: '4rem', marginTop: '2rem', maxWidth: '500px' }}
+        >
+          <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', marginBottom: '0.5rem' }}>
+            {section.title}
+          </Typography>
+          <List sx={{ padding: 0 }}>
+            {section.content.map((point, idx) => (
+              <ListItem
+                key={idx}
+                sx={{
+                  display: 'list-item',
+                  paddingLeft: '1rem',
+                  typography: 'body1',
+                  wordBreak: 'break-word',
+                }}
+              >
+                {point}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       ))}
     </Box>
   );
